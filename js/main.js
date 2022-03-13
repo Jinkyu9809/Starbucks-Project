30dca7e175371b5d6244546a763d11609b0dd03b
JinKyu Kim <wlsrb9809@naver.com>
2022-03-14 01:32:35 +0900

    First commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  // Logic..
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  // Html의 속성을 지정할때 쓰인다.
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
