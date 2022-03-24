fb1921708e58394f0131085cae91655b7d0d6f9d
jinkyu9809git config -- global user .password  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 <wlsrb9809@naver.com>
2022-03-24 23:55:22 +0900

    commit e

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ new Swiper('.notice-line .swiper-container', {
 new Swiper('.promotion .swiper-container', {
   slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
-  centeredSlides: true, // 1번 슬라이드가 가운데 보아기
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true ,
   autoplay: {
     delay: 5000
